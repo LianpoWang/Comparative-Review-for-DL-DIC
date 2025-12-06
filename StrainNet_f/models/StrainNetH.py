d24c1f777e632c2b3d84f1da4e809fb455945997
LianpoWang <145277523+LianpoWang@users.noreply.github.com>
2025-12-06 15:15:23 +0800

    Add files via upload

---
+++ StrainNet_f/models/StrainNetH.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+from torch.nn.init import kaiming_normal_, constant_
+from .util import conv, predict_flow, deconv, crop_like
+
+__all__ = [
+    'StrainNet_h', 'StrainNet_h_bn'
+]
+
+
+class StrainNetH(nn.Module):
+    expansion = 1
+
+    def __init__(self,batchNorm=True):
+        super(StrainNetH,self).__init__()
+
+        self.batchNorm = batchNorm
+        self.conv1   = conv(self.batchNorm,   6,   64, kernel_size=7, stride=1)#第一个卷积层的步长为1，图像的分辨率除边缘基本可以保持不变
+        self.conv2   = conv(self.batchNorm,  64,  128, kernel_size=5, stride=2)
+        self.conv3   = conv(self.batchNorm, 128,  256, kernel_size=5, stride=2)
+        self.conv3_1 = conv(self.batchNorm, 256,  256)#输入通道数和输出通道数都是256，继续提取特征
+        self.conv4   = conv(self.batchNorm, 256,  512, stride=2)
+        self.conv4_1 = conv(self.batchNorm, 512,  512)
+        self.conv5   = conv(self.batchNorm, 512,  512, stride=2)
+        self.conv5_1 = conv(self.batchNorm, 512,  512)
+        self.conv6   = conv(self.batchNorm, 512, 1024, stride=2)
+        self.conv6_1 = conv(self.batchNorm,1024, 1024)
+
+        self.deconv5 = deconv(1024,512)#将特征图的通道数从 1024 减少到 512，并通过反卷积恢复一些空间分辨率
+        self.deconv4 = deconv(1026,256)
+        self.deconv3 = deconv(770,128)
+        self.deconv2 = deconv(386,64)
+
+        self.predict_flow6 = predict_flow(1024)
+        self.predict_flow5 = predict_flow(1026)
+        self.predict_flow4 = predict_flow(770)
+        self.predict_flow3 = predict_flow(386)
+        self.predict_flow2 = predict_flow(194)
+
+        self.upsampled_flow6_to_5 = nn.ConvTranspose2d(2, 2, 4, 2, 1, bias=False)
+        self.upsampled_flow5_to_4 = nn.ConvTranspose2d(2, 2, 4, 2, 1, bias=False)
+        self.upsampled_flow4_to_3 = nn.ConvTranspose2d(2, 2, 4, 2, 1, bias=False)
+        self.upsampled_flow3_to_2 = nn.ConvTranspose2d(2, 2, 4, 2, 1, bias=False)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                kaiming_normal_(m.weight, 0.1)
+                if m.bias is not None:
+                    constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                constant_(m.weight, 1)
+                constant_(m.bias, 0)
+
+    def forward(self, x):
+        out_conv2 = self.conv2(self.conv1(x))
+        out_conv3 = self.conv3_1(self.conv3(out_conv2))
+        out_conv4 = self.conv4_1(self.conv4(out_conv3))
+        out_conv5 = self.conv5_1(self.conv5(out_conv4))
+        out_conv6 = self.conv6_1(self.conv6(out_conv5))
+
+        flow6       = self.predict_flow6(out_conv6)
+        flow6_up    = crop_like(self.upsampled_flow6_to_5(flow6), out_conv5)
+        out_deconv5 = crop_like(self.deconv5(out_conv6), out_conv5)
+
+        concat5 = torch.cat((out_conv5,out_deconv5,flow6_up),1)
+        flow5       = self.predict_flow5(concat5)
+        flow5_up    = crop_like(self.upsampled_flow5_to_4(flow5), out_conv4)
+        out_deconv4 = crop_like(self.deconv4(concat5), out_conv4)
+
+        concat4 = torch.cat((out_conv4,out_deconv4,flow5_up),1)
+        flow4       = self.predict_flow4(concat4)
+        flow4_up    = crop_like(self.upsampled_flow4_to_3(flow4), out_conv3)
+        out_deconv3 = crop_like(self.deconv3(concat4), out_conv3)
+
+        concat3 = torch.cat((out_conv3,out_deconv3,flow4_up),1)
+        flow3       = self.predict_flow3(concat3)
+        flow3_up    = crop_like(self.upsampled_flow3_to_2(flow3), out_conv2)
+        out_deconv2 = crop_like(self.deconv2(concat3), out_conv2)
+
+        concat2 = torch.cat((out_conv2,out_deconv2,flow3_up),1)
+        flow2 = self.predict_flow2(concat2)
+
+        if self.training:
+            return flow2,flow3,flow4,flow5,flow6
+        else:
+            return flow2
+
+    def weight_parameters(self):
+        return [param for name, param in self.named_parameters() if 'weight' in name]
+
+    def bias_parameters(self):
+        return [param for name, param in self.named_parameters() if 'bias' in name]
+
+
+def StrainNet_h(data=None):
+
+    model = StrainNetH(batchNorm=False)
+    if data is not None:
+        state_dict = data['state_dict']
+        new_state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+        # 加载修正后的 state_dict
+        model.load_state_dict(new_state_dict)
+        #model.load_state_dict(data['state_dict'])
+    return model
+
+
+def StrainNet_h_bn(data=None):
+
+    model = StrainNetH(batchNorm=True)
+    if data is not None:
+        model.load_state_dict(data['state_dict'])
+    return model
